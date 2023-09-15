484914252f38d02e30dcef1494c3b3fd2c986a26
Марина Зубцова <87470059+SadGolem@users.noreply.github.com>
2023-09-15 12:14:21 +0700

    1

---
+++ FileSystem/Program.cs
@@ -0,0 +1,56 @@
+﻿Console.WriteLine("Введите 1 - вывести данные на экран, введите 2 - заполнить данные и добавить новую запись.");
+
+string input = Console.ReadLine();
+string path = @"D:\Project_Visual\Skillbox\6\File-System-Skillbox6\FileSystem Skillbox6\FileSystem Skillbox6\bin\Debug\net7.0\data.txt";
+
+if (input == "2")
+{
+    int id;
+    if (File.Exists(path))
+    {
+        id = File.ReadLines(path).Count();
+    }
+    else
+    {
+        id = 0;
+    }
+    StreamWriter streamWriter = new StreamWriter("data.txt", true); //создание потока для работы с файлом
+
+
+
+    streamWriter.Write(id + "#");
+
+
+    streamWriter.Write(DateTime.Now + "#");
+
+    Console.WriteLine("Введите ФИО");
+    streamWriter.Write(Console.ReadLine() + "#");
+
+    Console.WriteLine("Введите возраст");
+    streamWriter.Write(Console.ReadLine() + "#");
+
+    Console.WriteLine("Введите дату рождения");
+    streamWriter.Write(Console.ReadLine() + "#");
+
+    Console.WriteLine("Введите место рождения");
+    streamWriter.WriteLine(Console.ReadLine() + "#");
+
+    streamWriter.Flush(); //очистка буфера 
+    streamWriter.Close();
+}
+else if (input == "1")
+{
+    using (StreamReader streamReader = new StreamReader("data.txt"))
+    {
+        while (!streamReader.EndOfStream)
+        {
+            Console.WriteLine(streamReader.ReadLine());
+        }
+        streamReader.Close();
+    }
+
+}
+else
+{
+    Console.WriteLine("Неизвестный ввод.");
+}
